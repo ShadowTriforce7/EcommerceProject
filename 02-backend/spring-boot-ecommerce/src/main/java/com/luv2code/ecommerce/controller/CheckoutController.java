2ee9ade97d1f47734c82a4f043c70933527f5212
Hadi Channaa <aahdi600@gmail.com>
2022-04-27 12:34:32 -0500

    added stripe API

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -1,19 +1,28 @@
 package com.luv2code.ecommerce.controller;
 
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 //@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
 
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
 	private CheckoutService checkoutService;
 	
 	public CheckoutController(CheckoutService checkoutService) {
@@ -25,7 +34,18 @@ public class CheckoutController {
 		PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 		return purchaseResponse;
 	}
-	
+
+	@PostMapping("/payment-intent")
+	public ResponseEntity<String> createPaymnetIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+		
+		logger.info("paymentInfo.amount: " + paymentInfo.getAmount());
+		
+		PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+		
+		String paymentStr = paymentIntent.toJson();
+		
+		return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+	}
 	
 	
 }
--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -4,12 +4,14 @@ import { Router } from '@angular/router';
 import { Country } from 'src/app/common/country';
 import { Order } from 'src/app/common/order';
 import { OrderItem } from 'src/app/common/order-item';
+import { PaymentInfo } from 'src/app/common/payment-info';
 import { Purchase } from 'src/app/common/purchase';
 import { State } from 'src/app/common/state';
 import { CartService } from 'src/app/services/cart.service';
 import { CheckoutService } from 'src/app/services/checkout.service';
 import { Luv2ShopFormService } from 'src/app/services/luv2-shop-form.service';
 import { Luv2ShopValidators } from 'src/app/validators/luv2-shop-validators';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-checkout',
@@ -33,6 +35,15 @@ export class CheckoutComponent implements OnInit {
 
   storage: Storage = sessionStorage;
 
+  //initialize Stripe API
+  stripe = Stripe(environment.stripePublishableKey);
+
+  paymentInfo: PaymentInfo = new PaymentInfo();
+  cardElement: any;
+  displayError: any = "";
+
+  isDisabled: boolean = false;
+
   constructor(private formBuilder: FormBuilder,
               private luv2ShopFormService: Luv2ShopFormService,
               private cartService: CartService,
@@ -40,6 +51,10 @@ export class CheckoutComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit(): void {
+
+    //setup Stripe Payment form
+    this.setupStripePaymentForm();
+
 
     this.reviewCartDetails();
 
@@ -66,6 +81,7 @@ export class CheckoutComponent implements OnInit {
         country: new FormControl('', [Validators.required]),
         zipCode: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace])
       }),
+      /*
       creditCard: this.formBuilder.group({
         cardType: new FormControl('', [Validators.required]),
         nameOnCard: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
@@ -73,9 +89,10 @@ export class CheckoutComponent implements OnInit {
         securityCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}$')]),
         expirationMonth: [''],
         expirationYear: ['']
-      })
+      })*/
     });
 
+    /*
     //populate credit card months
 
     const startMonth: number = new Date().getMonth() + 1;
@@ -95,6 +112,7 @@ export class CheckoutComponent implements OnInit {
         this.creditCardYears = data;
       }
     )
+    */
 
     //populate countries
     this.luv2ShopFormService.getCountries().subscribe(
@@ -104,6 +122,32 @@ export class CheckoutComponent implements OnInit {
       }
     );
 
+  }
+
+  setupStripePaymentForm() {
+    //get a handle to stripe elements
+    var elements = this.stripe.elements();
+
+    // create a card element
+    this.cardElement = elements.create('card', { hidePostalCode: true});
+
+    //Add an instance of card UI component into the 'card-element' div
+    this.cardElement.mount('#card-element');
+
+    // add event binding for the 'change' event on the card element
+    this.cardElement.on('change', (event) => {
+      // get a handle to card-errors element
+      this.displayError = document.getElementById('card-errors');
+
+      if(event.complete) {
+        this.displayError.textContent = "";
+      } else if (event.error) {
+        // show validation error to customer
+        this.displayError.textContent = event.error.message;
+      }
+
+
+    })
   }
 
   reviewCartDetails() {
@@ -204,21 +248,69 @@ export class CheckoutComponent implements OnInit {
     //console.log("The shipping address country is " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
     //console.log("The shipping address state is " + this.checkoutFormGroup.get('shippingAddress').value.state.name);
 
-    //call REST API via the CheckoutService
-    this.checkoutService.placeOrder(purchase).subscribe(
-      {
-        next: response => {
-          alert(`Your order has been recieved.\nOrder tracking number: ${response.orderTrackingNumber}`);
+    //compute payment info
+    this.paymentInfo.amount = Math.round(this.totalPrice * 100);
+    this.paymentInfo.currency = "USD";
+    this.paymentInfo.receiptEmail = purchase.customer.email;
 
-          //reset cart
-          this.resetCart();
+    console.log(`this.paymentInfo.amount: ${this.paymentInfo.amount}`);
 
-        },
-        error: err => {
-          alert(`There was an error: ${err.message}`);
+    //if valid form then
+    // - create payment intent
+    // - confirm card payment
+    // - place order
+
+    if(!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
+
+      this.isDisabled = true;
+
+      this.checkoutService.createPaymentInent(this.paymentInfo).subscribe(
+        (paymentIntentResponse) => {
+          this.stripe.confirmCardPayment(paymentIntentResponse.client_secret,
+            {
+              payment_method: {
+                card: this.cardElement,
+                billing_details: {
+                  email: purchase.customer.email,
+                  name: `${purchase.customer.firstName} ${purchase.customer.lastName}`,
+                  address: {
+                    line1: purchase.billingAddress.street,
+                    city: purchase.billingAddress.city,
+                    state: purchase.billingAddress.state,
+                    postal_code: purchase.billingAddress.zipCode,
+                    country: this.billingAddressCountry.value.code
+                  }
+                }
+              }
+            }, { handleActions: false})
+          .then(function(result) {
+            if (result.error) {
+              //inform the customer there was an error
+              alert(`There was an error: ${result.error.message}`);
+              this.isDisabled = false;
+            } else {
+              //call REST API via the CheckoutService
+              this.checkoutService.placeOrder(purchase).subscribe({
+                next: response => {
+                  alert(`Your order has been recieved. \nOrder tracking number: ${response.orderTrackingNumber}`);
+
+                  //reset cart
+                  this.resetCart();
+                  this.isDisabled = false;
+                },
+                error :err => {
+                  alert(`There was an error: ${err.message}`);
+                  this.isDisabled = false;
+                }
+              })
+            }
+          }.bind(this));
         }
-      }
-    );
+      );
+    } else {
+      this.checkoutFormGroup.markAllAsTouched();
+      return;
+    }
     
   }
 
@@ -227,6 +319,7 @@ export class CheckoutComponent implements OnInit {
     this.cartService.cartItems = [];
     this.cartService.totalPrice.next(0);
     this.cartService.totalQuantity.next(0);
+    this.cartService.persistCartItems();
 
     //reset the form data
     this.checkoutFormGroup.reset();
