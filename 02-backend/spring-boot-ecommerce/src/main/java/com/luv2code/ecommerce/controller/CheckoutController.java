55c075d1e42d184ecc77b0b63915507aa9f80f8b
Joseph James <josephjames9405@gmail.com>
2022-04-27 14:55:07 -0400

    testing

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -1,5 +1,6 @@
 package com.luv2code.ecommerce.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
