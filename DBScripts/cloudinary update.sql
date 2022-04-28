3cddb832cc21a8eaf11ea034638bf88ca4b1f08d
Hadi Channaa <aahdi600@gmail.com>
2022-04-28 13:53:14 -0500

    added images to cloudinary

---
+++ DBScripts/cloudinary update.sql
@@ -0,0 +1,89 @@
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170358/products/drinks/drink-20_yy1fp6.png' WHERE (`id` = '14');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170357/products/drinks/drink-19_ouedos.png' WHERE (`id` = '13');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170356/products/drinks/drink-18_lc3cs4.png' WHERE (`id` = '12');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170356/products/drinks/drink-17_nnvamz.png' WHERE (`id` = '11');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170359/products/drinks/drink-16_hicvie.png' WHERE (`id` = '10');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170357/products/drinks/drink-15_ckwxit.png' WHERE (`id` = '9');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170355/products/drinks/drink-14_fzsm3r.png' WHERE (`id` = '8');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170358/products/drinks/drink-13_zpvxx9.png' WHERE (`id` = '7');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170355/products/drinks/drink-12_f6mcsk.png' WHERE (`id` = '6');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170359/products/drinks/drink-11_xbjvdu.png' WHERE (`id` = '5');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170356/products/drinks/drink-10_gepvqt.png' WHERE (`id` = '4');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170356/products/drinks/drink-09_cwhicj.png' WHERE (`id` = '3');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170355/products/drinks/drink-08_jza5ez.png' WHERE (`id` = '2');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170354/products/drinks/drink-07_xfyhk8.png' WHERE (`id` = '1');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170354/products/drinks/drink-06_v01adp.png' WHERE (`id` = '20');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170354/products/drinks/drink-05_djbugv.png' WHERE (`id` = '19');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170354/products/drinks/drink-04_kkkua1.png' WHERE (`id` = '18');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170354/products/drinks/drink-03_r7gmxq.png' WHERE (`id` = '17');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170355/products/drinks/drink-02_dcepvp.png' WHERE (`id` = '16');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170354/products/drinks/drink-01_boxmo6.png' WHERE (`id` = '15');
+
+
+
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170397/products/Spices/spice-01_a6guaz.png' WHERE (`id` = '21');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170409/products/Spices/spice-02_rkpbct.png' WHERE (`id` = '22');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170397/products/Spices/spice-03_jjwdg5.png' WHERE (`id` = '23');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170405/products/Spices/spice-04_uplufr.png' WHERE (`id` = '24');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170400/products/Spices/spice-05_qwzebg.png' WHERE (`id` = '25');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170400/products/Spices/spice-06_yl19u2.png' WHERE (`id` = '26');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170402/products/Spices/spice-07_gujs0t.png' WHERE (`id` = '27');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170397/products/Spices/spice-08_jmomev.png' WHERE (`id` = '28');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170397/products/Spices/spice-09_abhfx8.png' WHERE (`id` = '29');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170402/products/Spices/spice-10_tq3jj1.png' WHERE (`id` = '30');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170397/products/Spices/spice-11_j8x74c.png' WHERE (`id` = '31');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170408/products/Spices/spice-12_nfjbka.png' WHERE (`id` = '32');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170400/products/Spices/spice-13_ovld9j.png' WHERE (`id` = '33');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170407/products/Spices/spice-14_u8wjzk.png' WHERE (`id` = '34');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170413/products/Spices/spice-15_antcy8.png' WHERE (`id` = '35');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170409/products/Spices/spice-16_eilpm3.png' WHERE (`id` = '36');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170398/products/Spices/spice-17_inlvwg.png' WHERE (`id` = '37');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170401/products/Spices/spice-18_gmeyyw.png' WHERE (`id` = '38');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170396/products/Spices/spice-19_imx8hk.png' WHERE (`id` = '39');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170405/products/Spices/spice-20_iulj5h.png' WHERE (`id` = '40');
+
+
+
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170380/products/fruits/apple_xsvply.png' WHERE (`id` = '41');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170381/products/fruits/apricot_dby1lk.png' WHERE (`id` = '42');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170377/products/fruits/banana_lzv9cp.png' WHERE (`id` = '43');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170377/products/fruits/cherry_qqpwpn.png' WHERE (`id` = '44');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170380/products/fruits/dragonfruit_ddwiea.png' WHERE (`id` = '45');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170376/products/fruits/fig_uxults.png' WHERE (`id` = '46');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170381/products/fruits/grapefruit_wukvtb.png' WHERE (`id` = '47');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170381/products/fruits/greengrape_ruruwp.png' WHERE (`id` = '48');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170384/products/fruits/jackfruit_qeme0r.png' WHERE (`id` = '49');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170385/products/fruits/kiwi_nelusn.png' WHERE (`id` = '50');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170377/products/fruits/mango_s5k7z1.png' WHERE (`id` = '51');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170384/products/fruits/orange_lplt3z.png' WHERE (`id` = '52');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170376/products/fruits/pear_batvlu.png' WHERE (`id` = '53');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170377/products/fruits/pineapple_dumduo.png' WHERE (`id` = '54');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170384/products/fruits/plum_rzdder.png' WHERE (`id` = '55');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170383/products/fruits/pomegranate_atgenf.png' WHERE (`id` = '56');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170378/products/fruits/purplegrape_wqz0yn.png' WHERE (`id` = '57');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170379/products/fruits/raspberry_xkzbwy.png' WHERE (`id` = '58');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170386/products/fruits/strawberry_hupbcq.png' WHERE (`id` = '59');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170383/products/fruits/watermelon_tp61aw.png' WHERE (`id` = '60');
+
+
+
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170415/products/vegetables/asparagus_gawrwr.png' WHERE (`id` = '61');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170402/products/vegetables/beans_cozcoz.png' WHERE (`id` = '62');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170409/products/vegetables/beetroot_e8i5yy.png' WHERE (`id` = '63');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170404/products/vegetables/bellpepper_ccgynq.png' WHERE (`id` = '64');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170413/products/vegetables/broccoli_oj7wzd.png' WHERE (`id` = '65');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170405/products/vegetables/cabbage_ze8iyf.png' WHERE (`id` = '66');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170403/products/vegetables/carrot_ejqmz5.png' WHERE (`id` = '67');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170413/products/vegetables/cauliflower_pwt88y.png' WHERE (`id` = '68');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170405/products/vegetables/celery_jyjzcx.png' WHERE (`id` = '80');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170399/products/vegetables/cucumber_q1t3ar.png' WHERE (`id` = '69');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170411/products/vegetables/eggplant_efi3ht.png' WHERE (`id` = '70');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170415/products/vegetables/kale_silzaj.png' WHERE (`id` = '71');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170401/products/vegetables/lettuce_sftet9.png' WHERE (`id` = '72');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170402/products/vegetables/onion_nncdvl.png' WHERE (`id` = '73');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170404/products/vegetables/potato_l2o7nq.png' WHERE (`id` = '74');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170399/products/vegetables/pumpkin_e9sywt.png' WHERE (`id` = '75');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170397/products/vegetables/radish_mmnqx7.png' WHERE (`id` = '76');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170412/products/vegetables/spinach_vykwwp.png' WHERE (`id` = '77');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170412/products/vegetables/springonion_doisdd.png' WHERE (`id` = '78');
+UPDATE `full-stack-ecommerce`.`product` SET `image_url` = 'https://res.cloudinary.com/nooks-cranny/image/upload/v1651170409/products/vegetables/tomato_nepkds.png' WHERE (`id` = '79');
