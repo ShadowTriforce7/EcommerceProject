2cfc85ab3f2eae5a36113ba7b7e32d6773394c05
Hadi Channaa <50154712+ShadowTriforce7@users.noreply.github.com>
2022-05-02 11:46:27 -0500

    Update my-app-config.ts

--- angular-ecommerce/src/app/config/my-app-config.ts
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oa4msf9q534IfO0o5d7',
         issuer: 'https://dev-5964265.okta.com/oauth2/default',
-        redirectUri: 'https://localhost:4200/login/callback',
+        redirectUri: 'https://nookscranny.azurewebsites.net/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
