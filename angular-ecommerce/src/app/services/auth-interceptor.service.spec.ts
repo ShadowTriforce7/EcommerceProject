f9a95270f66be1d564f578c0bbbd4416565e15b4
Hadi Channaa <aahdi600@gmail.com>
2022-05-03 10:48:24 -0500

    tried to change pom

--- angular-ecommerce/src/app/services/auth-interceptor.service.spec.ts
+++ angular-ecommerce/src/app/services/auth-interceptor.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { OktaAuthService } from '@okta/okta-angular';
 import { AuthInterceptorService } from './auth-interceptor.service';
 
 describe('AuthInterceptorService', () => {
   let service: AuthInterceptorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[OktaAuthService]
+    });
     service = TestBed.inject(AuthInterceptorService);
   });
 
